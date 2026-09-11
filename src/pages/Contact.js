b9b3d7b660b64b35afb09289f8ad39bae04d9357
cesar <cesarxx_19@hotmail.com>
2026-09-11 18:52:02 -0500

    cuarto deploy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { destinations } from '../data/content';
 
-const API_URL = import.meta.env.VITE_API_URL;
+//const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Contact() {
   const [form, setForm] = useState({ name: '', email: '', phone: '', destination: '', message: '' });
